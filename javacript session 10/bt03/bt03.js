ca58acd4d49a9d36e5826f01a1e11b8c911a3123
Nhatran25101996 <nhatran.svns@gmail.com>
2025-05-28 22:49:50 +0900

    Add files via upload

---
+++ javacript session 10/bt03/bt03.js
@@ -0,0 +1,6 @@
+let width = Number(prompt('Nhập chiều dài:', '10'));
+let height = Number(prompt('Nhập chiều rộng:', '5'));
+let area = width * height;
+
+console.log(area);
+document.write('Diện tích là: ' + area);
---
+++ javacript session 10/bt04/bt04.js
@@ -0,0 +1,7 @@
+
+let firstNumber = Number(prompt('Nhập vào số thứ nhất:', '10'));
+let secondNumber = Number(prompt('Nhập vào số thứ hai:', '5'));
+let sum = firstNumber + secondNumber;
+
+alert('Tổng hai số là: ' + sum);
+console.log(sum);
